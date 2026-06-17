cab51fd89d228d3c622a564602a281d598532839
eliteserving <erimrank167@gmail.com>
2026-06-17 23:06:07 +0300

    Updates

--- app/src/main/java/com/name/app/BootReceiver.java
+++ app/src/main/java/com/name/app/BootReceiver.java
@@ -1,14 +1,25 @@
 package com.example.ussdwebview;
 
+
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
+import android.os.Handler;
+import android.os.Looper;
+
+
 
 public class BootReceiver extends BroadcastReceiver {
 
+
+
     @Override
-    public void onReceive(Context context,Intent intent){
+    public void onReceive(
+            Context context,
+            Intent intent
+    ){
+
 
 
         if(Intent.ACTION_BOOT_COMPLETED.equals(
@@ -16,35 +27,127 @@ public class BootReceiver extends BroadcastReceiver {
         )){
 
 
-            Intent service =
-                    new Intent(
-                            context,
-                            KeepAliveService.class
+
+            startService(
+                    context
+            );
+
+
+
+            Handler handler =
+                    new Handler(
+                            Looper.getMainLooper()
                     );
 
 
-            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+
+            // retry after 1 minute
+
+            handler.postDelayed(
+                    new Runnable(){
+
+                        @Override
+                        public void run(){
 
 
-                context.startForegroundService(
-                        service
-                );
+                            startService(
+                                    context
+                            );
 
 
-            }else{
+                        }
 
 
-                context.startService(
-                        service
-                );
+                    },
 
-            }
+                    60 * 1000
+
+            );
+
+
+
+
+
+
+
+            // retry again after 5 minutes
+
+            handler.postDelayed(
+                    new Runnable(){
+
+                        @Override
+                        public void run(){
+
+
+                            startService(
+                                    context
+                            );
+
+
+                        }
+
+
+                    },
+
+                    5 * 60 * 1000
+
+            );
+
 
 
         }
 
 
     }
+
+
+
+
+
+
+
+
+    private void startService(
+            Context context
+    ){
+
+
+
+        Intent service =
+
+                new Intent(
+                        context,
+                        KeepAliveService.class
+                );
+
+
+
+
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+
+
+
+            context.startForegroundService(
+                    service
+            );
+
+
+
+        }else{
+
+
+
+            context.startService(
+                    service
+            );
+
+
+        }
+
+
+    }
+
 
 
 }
